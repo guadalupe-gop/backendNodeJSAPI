38416ab31cab0038b49ed58c5d4dfb78d3ef1799
Guadalupe Gomez <g.gop112019@gmail.com>
2021-10-20 18:46:55 -0500

    routing con express

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola, mi server en express!');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('soy una nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Volt Energy 473ml',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port ' + port);
 });

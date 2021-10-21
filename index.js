2c46d31631c611ca023638a591604f2e333fe59e
Guadalupe Gomez <g.gop112019@gmail.com>
2021-10-20 23:46:34 -0500

    GET: recibiendo parametros

--- index.js
+++ index.js
@@ -10,10 +10,36 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('soy una nueva ruta');
 });
 
-app.get('/productos', (req, res) => {
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Volt Energy 473ml',
+      price: 1000,
+    },
+    {
+      name: 'Redbull blue',
+      price: 1000,
+    },
+  ]);
+});
+
+// devolviendo el detalle de un producto, recibiendo el id
+app.get('/products/:id', (req, res) => {
+  // recogemos el id
+  const { id } = req.params;
   res.json({
+    id,
     name: 'Volt Energy 473ml',
     price: 1000,
+  });
+});
+
+// un endpoint más complejo recibiendo dos parametros
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 

4da551d894e45a9f20054e379d461bca8c90e7b9
Guadalupe Gomez <g.gop112019@gmail.com>
2021-10-25 15:11:36 -0500

    POST: metodo para crear

--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-// const router = require('./productsRouter');
 const productsRouter = require('./productsRouter');
 // const usersRouter = require('./usersRouter');
 
 function routerApi(app) {
   const router = express.Router();
-  app.use('/api/v1', router);
   // con esto generamos un path global
+  app.use('/api/v1', router);
   router.use('/products', productsRouter);
+
+  // podemos crear otra version diferente de modo que no choquen entre si
+  // const router = express.Router();
+  // app.use('/api/v2', router);
+
+  // forma normal como se haria
   // app.use('/api/v1/products', productsRouter);
   // app.use('/users', usersRouter);
 }
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const productsRouter = require('./productsRouter');
-const categoriesRouter = require('./categoriesRouter');
-
-function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/categories', categoriesRouter);
-}
-
-module.exports = routerApi;

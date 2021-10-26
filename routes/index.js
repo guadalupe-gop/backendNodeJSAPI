b28e325f36c12f38defd0eaf394f866f8233819e
@gopdev <markus44p@gmail.com>
2021-10-25 21:51:32 -0500

    Recibiendo cambios de...

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+const express = require('express');
 const productsRouter = require('./productsRouter');
 const categoriesRouter = require('./categoriesRouter');
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/categories', categoriesRouter);
+  // creamos una ruta maestra
+  const router = express.Router();
+  app.use('/api/v1', router);
+  // app.use('/api/v1/products', productsRouter);
+  // app.use('/api/v1/categories', categoriesRouter);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;

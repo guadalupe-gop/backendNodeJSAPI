2065a2aeb61404b78e0d68e9b74ad52fb831d4c5
Guadalupe Gomez <g.gop112019@gmail.com>
2021-10-25 21:48:56 -0500

    put patch delete

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -40,4 +40,24 @@ router.post('/', (req, res) => {
   });
 });
 
+// METODO PATCH
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+// DELETE
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
 module.exports = router;

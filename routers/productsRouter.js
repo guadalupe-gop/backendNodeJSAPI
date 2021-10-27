1749276f70a1d38908492c43340e1b3667aa6987
Guadalupe Gomez <g.gop112019@gmail.com>
2021-10-27 11:20:37 -0500

    Async await y captura de errores con try-catch

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -15,32 +15,38 @@ router.get('/filter', (req, res) => {
 });
 
 // devolviendo el detalle de un producto, recibiendo el id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // recogemos el id
   const { id } = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.json(product);
 });
 
 // METODO POST
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
   res.status(201).json({ newProduct });
 });
 
 // METODO PATCH enviar parcial
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = service.update(id, body);
-  res.json(product);
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 });
 
 // DELETE
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const rta = service.delete(id);
+  const rta = await service.delete(id);
   res.json(rta);
 });
 module.exports = router;
--- services/productService.js
+++ services/productService.js
@@ -32,7 +32,7 @@ class ProductsService {
   //   return newProduct;
   // }
 
-  create({ name, price, image }) {
+  async create({ name, price, image }) {
     const newProduct = {
       id: faker.datatype.uuid(),
       // ...data,
@@ -44,15 +44,19 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find((item) => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
@@ -66,7 +70,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product not found');
